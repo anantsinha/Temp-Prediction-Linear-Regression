618a87b00b44c001fc511fbb78601c88763aa12f
Anant Sinha <anantsinha9811@gmail.com>
2020-07-16 00:48:05 +0530

    Add final comments

--- ClimateChange.R
+++ ClimateChange.R
@@ -1,4 +1,5 @@
-setwd("~/Developer/Climate Change")
+setwd("~/Developer/Week2 - Linear Regression/Climate Change")
+# Linear Regression to predict Temperature using cocentration of pollutants
 #load climate data
 climate <- read.csv("climate_change.csv")
 # view structure
@@ -18,3 +19,19 @@ cor(training)
 # Clear that:
 #             N2O highly correlated with: CO2, CH4, CFC.12
 #             CFC.11 highly correlated with: CH4, CFC.12
+
+# lm Model with MEI, TSI, Aerosols and N2O only
+TempReg2 <- lm(Temp ~ N2O + MEI + TSI + Aerosols, data = training)
+summary(TempReg2)
+# Adjusted R Squared of this model - 0.7261, Adjusted R squared of prev model 0.7436
+# It makes more sense to use the second model, since difference in R squared is very small
+
+# produce model by AIC
+# Only CH4 was not included
+# Adjusted R squared 
+AicTempModel <- step(TempReg)
+predictedTemp <- predict(AicTempModel, newdata = testing)
+SSE <- sum((testing$Temp - predictedTemp)^2)
+SST <- sum ((mean(training$Temp) - testing$Temp)^2)
+R2 <- 1-SSE/SST
+# test R squared achieved  0.6286051
